5a9496c77e6b736e53508403d59d7726a3f17e4f
Vinicius <beneditoloura@gmail.com>
2024-02-15 07:08:03 -0300

    feat: adjust styles and create authenticated mock screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
-import { Button } from "@/src/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return <Button>Click</Button>;
+  return (
+    <div className="flex flex-col gap-y-4">
+      <div>This is a screen for authenticated users only</div>
+      <div>
+        <UserButton />
+      </div>
+    </div>
+  );
 }
