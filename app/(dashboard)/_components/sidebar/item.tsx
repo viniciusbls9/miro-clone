cd8aacc0eb68c3fc2870f61e7a2ef58b0f26b52f
Vinicius <beneditoloura@gmail.com>
2024-04-01 07:38:56 -0300

    feat: use hint

--- app/(dashboard)/_components/sidebar/item.tsx
+++ app/(dashboard)/_components/sidebar/item.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import { useOrganization, useOrganizationList } from "@clerk/nextjs";
 import { cn } from "@/src/lib/utils";
+import { Hint } from "@/src/components/hint";
 
 interface ItemProps {
   id: string;
@@ -24,16 +25,18 @@ export const Item = ({ id, name, imageUrl }: ItemProps) => {
 
   return (
     <div className="aspect-square relative">
-      <Image
-        src={imageUrl}
-        alt={name}
-        fill
-        onClick={onClick}
-        className={cn(
-          "rounded-md cursor-pointer opacity-75 hover:opacity-100",
-          isActive && "opacity-100"
-        )}
-      />
+      <Hint label={name} side="right" align="start" sideOffset={18}>
+        <Image
+          src={imageUrl}
+          alt={name}
+          fill
+          onClick={onClick}
+          className={cn(
+            "rounded-md cursor-pointer opacity-75 hover:opacity-100",
+            isActive && "opacity-100"
+          )}
+        />
+      </Hint>
     </div>
   );
 };
--- src/components/hint.tsx
+++ src/components/hint.tsx
@@ -23,19 +23,21 @@ export const Hint = ({
   sideOffset,
   alignOffset,
 }: HintProps) => {
-  <TooltipProvider>
-    <Tooltip delayDuration={100}>
-      <TooltipTrigger asChild>{children}</TooltipTrigger>
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={100}>
+        <TooltipTrigger asChild>{children}</TooltipTrigger>
 
-      <TooltipContent
-        className="text-white bg-black border-black"
-        side={side}
-        align={align}
-        sideOffset={sideOffset}
-        alignOffset={alignOffset}
-      >
-        <p className="font-semibold capitalize">{label}</p>
-      </TooltipContent>
-    </Tooltip>
-  </TooltipProvider>;
+        <TooltipContent
+          className="text-white bg-black border-black"
+          side={side}
+          align={align}
+          sideOffset={sideOffset}
+          alignOffset={alignOffset}
+        >
+          <p className="font-semibold capitalize">{label}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
 };
