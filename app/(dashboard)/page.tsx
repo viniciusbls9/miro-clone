5431dcb389966761b3d3753c1e1423973c49d1c2
Vinicius <beneditoloura@gmail.com>
2024-04-08 07:52:36 -0300

    feat: verify if show empty org or board list

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,17 @@
+"use client";
+
 import React from "react";
+import { EmptyOrg } from "./_components/empty-org";
+import { useOrganization } from "@clerk/nextjs";
 
 const DashboardPage: React.FC = () => {
-  return <div>Dashboard Root Page</div>;
+  const { organization } = useOrganization();
+
+  return (
+    <div className="flex-1 h-[calc(100%-80px)] p-6">
+      {!organization ? <EmptyOrg /> : <p>board list</p>}
+    </div>
+  );
 };
 
 export default DashboardPage;
