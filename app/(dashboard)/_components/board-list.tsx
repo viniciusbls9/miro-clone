facd190a3284e133cfbed69fcb16e9183acbbe2c
Vinicius <beneditoloura@gmail.com>
2024-04-10 07:16:58 -0300

    feat: create and use board list

---
+++ app/(dashboard)/_components/board-list.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { EmptyBoards } from "./empty-boards";
+import { EmptyFavorite } from "./empty-favorite";
+import { EmptySearch } from "./empty-search";
+
+interface BoardListProps {
+  orgId: string;
+  query: {
+    search?: string;
+    favorites?: string;
+  };
+}
+
+export const BoardList = ({ orgId, query }: BoardListProps) => {
+  const data = [];
+
+  if (!data?.length && query.search) {
+    return <EmptySearch />;
+  }
+
+  if (!data?.length && query.favorites) {
+    return <EmptyFavorite />;
+  }
+
+  if (!data?.length) {
+    return <EmptyBoards />;
+  }
+
+  return <div>{JSON.stringify(query)}</div>;
+};
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -3,13 +3,25 @@
 import React from "react";
 import { EmptyOrg } from "./_components/empty-org";
 import { useOrganization } from "@clerk/nextjs";
+import { BoardList } from "./_components/board-list";
 
-const DashboardPage: React.FC = () => {
+interface DashboardPageProps {
+  searchParams: {
+    search?: string;
+    favorites?: string;
+  };
+}
+
+const DashboardPage = ({ searchParams }: DashboardPageProps) => {
   const { organization } = useOrganization();
 
   return (
     <div className="flex-1 h-[calc(100%-80px)] p-6">
-      {!organization ? <EmptyOrg /> : <p>board list</p>}
+      {!organization ? (
+        <EmptyOrg />
+      ) : (
+        <BoardList orgId={organization.id} query={searchParams} />
+      )}
     </div>
   );
 };
