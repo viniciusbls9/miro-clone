8ec4d19995bf09a2544c5fa638fa49e5e51724e8
Vinicius <beneditoloura@gmail.com>
2024-04-11 07:19:07 -0300

    feat: create footer component

---
+++ app/(dashboard)/_components/board-card/Footer.tsx
@@ -0,0 +1,30 @@
+import { Star } from "lucide-react";
+
+import { cn } from "@/src/lib/utils";
+
+interface FooterProps {
+  isFavorite: boolean;
+  title: string;
+  authorLabel: string;
+  createdAtLabel: string;
+  onClick: () => void;
+  disabled: boolean;
+}
+
+export const Footer = ({
+  isFavorite,
+  title,
+  authorLabel,
+  createdAtLabel,
+  onClick,
+  disabled,
+}: FooterProps) => {
+  return (
+    <div className="relative bg-white p-3">
+      <p className="text-[13px] truncate max-w-[calc(100%-20px)">{title}</p>
+      <p className="opacity-0 group-hover:opacity-100 transition-opacity text-[11px] text-muted-foreground truncate">
+        {authorLabel}, {createdAtLabel}
+      </p>
+    </div>
+  );
+};
